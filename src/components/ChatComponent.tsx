604ab4b0a68aae4e804d1ff1071b166ef3ecef55
santheesh-14597 <santheesh.av@zohocorp.com>
2024-10-11 19:11:43 +0530

    Message chat box fixed

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React from "react";
+import { Input } from "./ui/input";
 import { useChat } from "ai/react";
+import { Button } from "./ui/button";
+import { Send } from "lucide-react";
+import MessageList from "./MessageList";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { Message } from "ai";
-import { cn } from "@/lib/utils";
 
 type Props = { chatId: number };
 
@@ -19,35 +22,53 @@ const ChatComponent = ({ chatId }: Props) => {
     },
   });
 
-  const { messages } = useChat({
+  const { input, handleInputChange, handleSubmit, messages } = useChat({
     api: "/api/chat",
-    body: { chatId },
+    body: {
+      chatId,
+    },
     initialMessages: data || [],
   });
-
+  React.useEffect(() => {
+    const messageContainer = document.getElementById("message-container");
+    if (messageContainer) {
+      messageContainer.scrollTo({
+        top: messageContainer.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [messages]);
   return (
-    <div className="relative max-h-screen overflow-scroll" id="message-container">
+    <div
+      className="relative max-h-screen overflow-scroll"
+      id="message-container"
+    >
+      {/* header */}
+      <div className="sticky top-0 inset-x-0 p-2 bg-white h-fit">
+        <h3 className="text-xl font-bold">Chat</h3>
+      </div>
+
+      {/* message list */}
       <MessageList messages={messages} isLoading={isLoading} />
-      {/* Form components */}
+
+      <form
+        onSubmit={handleSubmit}
+        className="sticky bottom-0 inset-x-0 px-2 py-4 bg-white"
+      >
+        <div className="flex">
+          <Input
+            value={input}
+            onChange={handleInputChange}
+            placeholder="Ask any question..."
+            className="w-full"
+          />
+          <Button className="bg-blue-600 ml-2">
+            <Send className="h-4 w-4" />
+          </Button>
+        </div>
+      </form>
     </div>
   );
 };
-
-type MessageListProps = {
-  isLoading: boolean;
-  messages: Message[];
-};
-
-const MessageList = ({ messages }: MessageListProps) => (
-  <div className="flex flex-col gap-2 px-4">
-    {messages.map((message) => (
-      <div key={message.id} className={cn("flex", { 'justify-end pl-10': message.role === 'user', 'justify-start pr-10': message.role === 'assistant' })}>
-        <div className={cn("rounded-lg px-3 text-sm py-1 shadow-md ring-1", { 'bg-blue-600 text-white': message.role === 'user' })}>
-          <p>{message.content}</p>
-        </div>
-      </div>
-    ))}
-  </div>
-);
 
 export default ChatComponent;
